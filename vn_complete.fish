202f01c4a8b36a9dc838ce51d747985885e5a733
Jose Luis Lafuente <jlesquembre@gmail.com>
2013-08-22 19:28:46 +0200

    Avoid function name collision

--- vn_complete.fish
+++ vn_complete.fish
@@ -1,7 +1,7 @@
 set vn_name vn
 
 
-function needs_command
+function __vn_needs_command
     set cmd (commandline -opc)
     if [ (count $cmd) -eq 1 -a $cmd[1] = "$vn_name" ]
         return 0
@@ -11,11 +11,11 @@ end
 
 
 
-function list_vn
+function __vn_list_vn
     eval "$vn_name ls --no-version"
 end
 
-function using_command
+function __vn_using_command
     set cmd (commandline -opc)
 	if test (count $cmd) -gt 1
 	    if test $argv[1] = $cmd[2]
@@ -25,7 +25,7 @@ function using_command
 	return 1
 end
 
-function using_command_options
+function __vn_using_command_options
     set cmd (commandline -opc)
 	if test (count $cmd) -gt 2
 	    if test $argv[1] = $cmd[2]
@@ -38,18 +38,18 @@ function using_command_options
 end
 
 #### new
-complete -f -c $vn_name -n 'needs_command' -a new -d 'Creates a new virtual environment'
-complete -f -c $vn_name -n 'using_command new' -s v -l version -d 'Version to install'
-complete -f -c $vn_name -n 'using_command_options new -v' -a 'latest'
+complete -f -c $vn_name -n '__vn_needs_command' -a new -d 'Creates a new virtual environment'
+complete -f -c $vn_name -n '__vn_using_command new' -s v -l version -d 'Version to install'
+complete -f -c $vn_name -n '__vn_using_command_options new -v' -a 'latest'
 
 
 #### workon
-complete -f -c $vn_name -n 'needs_command' -a workon -d 'Actives an existing virtual environment'
-complete -f -c $vn_name -n 'using_command workon' -a '(list_vn)' -d 'Node env'
+complete -f -c $vn_name -n '__vn_needs_command' -a workon -d 'Actives an existing virtual environment'
+complete -f -c $vn_name -n '__vn_using_command workon' -a '(list_vn)' -d 'Node env'
 
 #### rm
-complete -f -c $vn_name -n 'needs_command' -a rm -d 'Deletes a virtual environment'
-complete -f -c $vn_name -n 'using_command rm' -a '(list_vn)' -d 'Node env'
+complete -f -c $vn_name -n '__vn_needs_command' -a rm -d 'Deletes a virtual environment'
+complete -f -c $vn_name -n '__vn_using_command rm' -a '(list_vn)' -d 'Node env'
 
 #### ls
-complete -f -c $vn_name -n 'needs_command' -a ls -d 'List all existing virtual environments'
+complete -f -c $vn_name -n '__vn_needs_command' -a ls -d 'List all existing virtual environments'
